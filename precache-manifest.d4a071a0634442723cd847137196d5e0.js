95071d73c4b5cf023b9fbd9a517dc75807eadec7
rosechen <61014464+b2410832@users.noreply.github.com>
2020-06-30 13:20:02 +0800

    Updates

---
+++ precache-manifest.d4a071a0634442723cd847137196d5e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b0a0b7abe1f049b87ee16135140797",
+    "url": "/findmovies/index.html"
+  },
+  {
+    "revision": "90d86d605d2a78c1dd38",
+    "url": "/findmovies/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1660938ab9bfa15565f8",
+    "url": "/findmovies/static/css/main.5988299d.chunk.css"
+  },
+  {
+    "revision": "90d86d605d2a78c1dd38",
+    "url": "/findmovies/static/js/2.d604e352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/findmovies/static/js/2.d604e352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1660938ab9bfa15565f8",
+    "url": "/findmovies/static/js/main.fad18faf.chunk.js"
+  },
+  {
+    "revision": "02210a321b5da52c722b",
+    "url": "/findmovies/static/js/runtime-main.8c34006e.js"
+  },
+  {
+    "revision": "e7163f54c073f5d8964342a654b5ac1e",
+    "url": "/findmovies/static/media/cover.e7163f54.jpg"
+  }
+]);
